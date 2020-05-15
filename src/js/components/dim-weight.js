d2274685578ae6817196a318d27485963e43db0a
root <root@DESKTOP-98U85AD.localdomain>
2020-05-15 22:10:43 +1000

    draft working without imperial units

--- src/js/components/dim-weight.js
+++ src/js/components/dim-weight.js
@@ -8,6 +8,11 @@ var fHeight = document.getElementById("height");
 var fWeight = document.getElementById("weight");
 var fCube = document.querySelector(".cube");
 var dWeight = document.querySelector(".dim-weight")
+var calcsVolume = document.querySelector(".calcs__volume")
+var calcsVolRes = document.querySelector(".calcs__v-res")
+var calcsDimWeight = document.querySelector(".calcs__dim-weight")
+var calcsDimRes = document.querySelector(".calcs__dim-res")
+var calcsBilling = document.querySelector(".calcs__billing")
 var scaleWeight = document.querySelector(".weight-icon")
 var factorSlider = document.getElementById("factors")
 var dimFactor = factorSlider.value;
@@ -36,6 +41,10 @@ function updateCube() {
 	if(valid) {
 		// dimFactor = document.querySelector('input[name="factor"]:checked').value;
 		dimFactor = factorSlider.value;
+		document.querySelector("section.selected").classList.add('visually-hidden')
+		document.querySelectorAll(".selected").forEach((e) => e.classList.remove('selected'))
+		document.querySelectorAll(`.factor__${factorSlider.value}`).forEach((e) => e.classList.add('selected'))
+		document.querySelector("section.selected").classList.remove('visually-hidden')
 		let viewFactor = 1;
 		let maxDim = Math.max(fWidth.value,fHeight.value,fLength.value);
 		viewFactor = size/maxDim;
@@ -43,28 +52,32 @@ function updateCube() {
 		root.style.setProperty('--height',fHeight.value * viewFactor + "px");
 		root.style.setProperty('--length',fLength.value * viewFactor + "px");
 		// root.style.setProperty('--perspective',viewFactor*100 + "em");
-		let dimW = Math.round((fWidth.value * fHeight.value * fLength.value)/dimFactor);
+		let volume = fWidth.value * fHeight.value * fLength.value;
+		let dimW = Math.round((volume)/dimFactor);
 		dWeight.innerText = dimW
 		let actW = Math.round(fWeight.value)
 		scaleWeight.innerText = actW
 		//set the size to character width +2 + how far are we away from the next character...
+	
 		let dimWl = ((""+dimW).length+2) + Math.round(dimW/(dimW.toString().replace(/[0-9]/g,"9"))*100)/100
 		let actWl = ((""+actW).length+2) + Math.round(actW/(actW.toString().replace(/[0-9]/g,"9"))*100)/100
-		// dWeight.style.width = dimWl+"ch"
-		// scaleWeight.style.width = actWl+"ch"
-		// let dimWFirst = dimW.toString().charAt(0)
-		// let bFirst = b.toString().charAt(0)
-		// console.log((a.length+2)+"ch")
-		// debugger
+	
 		root.style.setProperty('--dimW-icon-size',((dimWl)) + "ch" )
 		root.style.setProperty('--weight-icon-size',((actWl)) + "ch")
-		// dWeight.style.height = (a.toString().charAt(0))
 		
+		calcsVolume.innerText = `${fWidth.value}*${fLength.value}*${fHeight.value}`
+		calcsVolRes.innerText =  volume
+		calcsDimWeight.innerText = `${volume}/${dimFactor}`
+		calcsDimRes.innerText = dimW
+		calcsBilling.innerText = actW
+		document.querySelector(".balanced-view").classList.remove("dim")
+		document.querySelector(".balanced-view").classList.remove("same")
 		if (dimW > actW) {
+			calcsBilling.innerText = dimW
 			document.querySelector(".balanced-view").classList.add("dim")
-		} else if (dimW < actW){
-			document.querySelector(".balanced-view").classList.remove("dim")
-		}		
+		} else if (Math.round(dimW) == Math.round(actW)){
+			document.querySelector(".balanced-view").classList.add("same")
+		}
 	}
 }
 
